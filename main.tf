bf0dd11a0e61c48c0c585f4277e2643d61b11383
Rajasekhar Chilaka <rajasekhar.chilaka@oracle.com>
2025-02-10 18:28:10 +0530

    codebuild.yaml 3

--- main.tf
+++ main.tf
@@ -143,6 +143,7 @@ resource "aws_codebuild_project" "codebuild" {
   name          = "reverse-ip-build"
   service_role  = aws_iam_role.codebuild.arn
   build_timeout = "5"
+
   artifacts {
     type = "NO_ARTIFACTS"
   }
@@ -163,7 +164,7 @@ resource "aws_codebuild_project" "codebuild" {
 
   source {
     type      = "GITHUB"
-    location  = "https://github.com/${local.config["github_repo"]}.git"
+    location  = "${local.config["github_repo"]}.git"
     buildspec = "buildspec.yml"
   }
 
