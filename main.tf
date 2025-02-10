03b25acba986303a6fc2213bd42afe13b7753a87
Rajasekhar Chilaka <rajasekhar.chilaka@oracle.com>
2025-02-10 19:42:22 +0530

    watch logs

--- main.tf
+++ main.tf
@@ -103,7 +103,68 @@ resource "aws_iam_role" "codebuild" {
       Action = "sts:AssumeRole"
     }]
   })
+    inline_policy {
+    name = "codebuild_execute_policy"
+    policy = data.aws_iam_policy_document.codebuild.json
+  }
 }
+
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    sid = "SSOCodebuildAllow"
+
+    actions = [
+      "s3:*",
+      "kms:*",
+      "ssm:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:DescribeTable",
+      "dynamodb:DescribeContinuousBackups",
+      "dynamodb:DescribeTimeToLive",
+      "dynamodb:ListTagsOfResource",
+    ]
+
+    resources = [
+      data.aws_ssm_parameter.lock_table_arn.value,
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+ 
+
+  statement {
+    actions = [
+      "identitystore:ListGroups",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
 
 resource "aws_iam_role_policy_attachment" "codebuild_policy" {
   role       = aws_iam_role.codebuild.name
@@ -155,7 +216,7 @@ resource "aws_codebuild_project" "codebuild" {
     environment_variable {
       name  = "AWS_ACCOUNT_ID"
       value = local.config["aws_account_id"]
-    }
+    } 
     environment_variable {
       name  = "AWS_REGION"
       value = local.config["aws_region"]
