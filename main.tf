38f2876879047e1e491c577b1c3fbdcfe0c5e4f6
Rajasekhar Chilaka <rajasekhar.chilaka@oracle.com>
2025-02-10 18:13:56 +0530

    codebuild.yaml

--- main.tf
+++ main.tf
@@ -138,3 +138,33 @@ output "ci_cd_role_arn" {
 output "debug_yaml" {
   value = local.config
 }
+
+resource "aws_codebuild_project" "codebuild" {
+  name          = "reverse-ip-build"
+  service_role  = aws_iam_role.codebuild.arn
+  build_timeout = "5"
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image          = "aws/codebuild/standard:5.0"
+    type           = "LINUX_CONTAINER"
+    privileged_mode = true
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = local.config["aws_account_id"]
+    }
+    environment_variable {
+      name  = "AWS_REGION"
+      value = local.config["aws_region"]
+    }
+  }
+
+  source {
+    type      = "GITHUB"
+    location  = "https://github.com/${local.config["github_repo"]}.git"
+    buildspec = "buildspec.yml"
+  }
+
+}
