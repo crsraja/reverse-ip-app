6069398c1f456b4c54e1731251d68ab326a576da
Rajasekhar Chilaka <rajasekhar.chilaka@oracle.com>
2025-02-14 15:25:01 +0530

    ecs

--- main.tf
+++ main.tf
@@ -49,6 +49,123 @@ resource "aws_ecr_repository" "ecr" {
   name = local.config["ecr_repository_name"]
 }
 
+resource "aws_iam_role" "codebuild" {
+  name = "CodeBuildServiceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  inline_policy {
+    name = "codepipeline_execute_policy"
+    policy = data.aws_iam_policy_document.codebuild.json
+  }
+
+
+}
+
+
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    actions = [
+      "s3:*",
+      "kms:*",
+      "ssm:*",
+      "secretsmanager:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetAuthorizationToken",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+      "ecr:*"
+      ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+}
+
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
+}
+
+
+resource "aws_codebuild_project" "codebuild" {
+  name          = "reverse-ip-build"
+  service_role  = aws_iam_role.codebuild.arn
+  build_timeout = "5"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image          = "aws/codebuild/standard:5.0"
+    type           = "LINUX_CONTAINER"
+    privileged_mode = true
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = local.config["aws_account_id"]
+    } 
+    environment_variable {
+      name  = "IMAGE_REPO_NAME"
+      value = local.config["ecr_repository_name"]
+    } 
+    
+    environment_variable {
+      name  = "IMAGE_TAG"
+      value = "latest"
+    } 
+    environment_variable {
+      name  = "AWS_REGION"
+      value = local.config["aws_region"]
+    }
+  }
+
+  source {
+    type      = "GITHUB"
+    location  = "${local.config["github_repo"]}.git"
+    buildspec = "buildspec.yml"
+  }
+
+}
+
+
+
+
 # ECS Task Execution Role
 resource "aws_iam_role" "ecs_task_execution" {
   name = "ECSTaskExecutionRole"
@@ -90,76 +207,6 @@ resource "aws_ecs_task_definition" "task" {
   }])
 }
 
-resource "aws_iam_role" "codebuild" {
-  name = "CodeBuildServiceRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "codebuild.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-
-  inline_policy {
-    name = "codepipeline_execute_policy"
-    policy = data.aws_iam_policy_document.codebuild.json
-  }
-
-
-}
-
-data "aws_iam_policy_document" "codebuild" {
-  statement {
-    actions = [
-      "s3:*",
-      "kms:*",
-      "ssm:*",
-      "secretsmanager:*",
-    ]
-
-    resources = [
-      "*",
-    ]
-  }
-
-  statement {
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-
-    resources = [
-      "*",
-    ]
-  }
-
-  statement {
-    actions = [
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:CompleteLayerUpload",
-      "ecr:GetAuthorizationToken",
-      "ecr:InitiateLayerUpload",
-      "ecr:PutImage",
-      "ecr:UploadLayerPart",
-      ]
-
-    resources = [
-      "*",
-    ]
-  }
-
-}
-
-
-resource "aws_iam_role_policy_attachment" "codebuild_policy" {
-  role       = aws_iam_role.codebuild.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
-}
 
 output "ecs_cluster_name" {
   description = "ECS Cluster Name"
@@ -188,44 +235,4 @@ output "ci_cd_role_arn" {
 
 output "debug_yaml" {
   value = local.config
-}
-
-resource "aws_codebuild_project" "codebuild" {
-  name          = "reverse-ip-build"
-  service_role  = aws_iam_role.codebuild.arn
-  build_timeout = "5"
-
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-  environment {
-    compute_type    = "BUILD_GENERAL1_SMALL"
-    image          = "aws/codebuild/standard:5.0"
-    type           = "LINUX_CONTAINER"
-    privileged_mode = true
-    environment_variable {
-      name  = "AWS_ACCOUNT_ID"
-      value = local.config["aws_account_id"]
-    } 
-    environment_variable {
-      name  = "IMAGE_REPO_NAME"
-      value = local.config["ecr_repository_name"]
-    } 
-    
-    environment_variable {
-      name  = "IMAGE_TAG"
-      value = "latest"
-    } 
-    environment_variable {
-      name  = "AWS_REGION"
-      value = local.config["aws_region"]
-    }
-  }
-
-  source {
-    type      = "GITHUB"
-    location  = "${local.config["github_repo"]}.git"
-    buildspec = "buildspec.yml"
-  }
-
 }
